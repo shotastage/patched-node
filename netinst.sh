cd93872dc49787f4e95a10744ad68bc870e4cf30
Shota Shimazu <shotastage@users.noreply.github.com>
2021-02-11 12:44:20 +0900

    update netinstaller

--- netinst.sh
+++ netinst.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/shotastage/patched-node.git
-cd patched-node
+mkdir tmp
+cd tmp
+echo "Downloading"
+curl -LJO https://github.com/shotastage/patched-node/releases/download/patched-v15.8.0/patched-node-15.8.0-arm64-macos.zip response -#
+echo "Unpacking..."
+unzip -d node patched-node-15.8.0-arm64-macos.zip >> /dev/null
+cd node
+chmod +x install.sh
 ./install.sh
 cd ..
-rm -rf patched-node
+cd ..
+rm -rf tmp
