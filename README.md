cf87ca229a34107d346e42a325ff39327b5cb750
Shota Shimazu <shotastage@users.noreply.github.com>
2021-02-11 11:35:42 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Patched Node.js 15.8.0
+
+This is 
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+is_macos?()
+{
+    if [ "$(uname)" == 'Darwin' ]; then
+        echo Running on macOS...
+    else
+        exit 1
+    fi
+}
+
+is_arm64?()
+{
+    if [ "$(uname -m)" == 'arm64' ]; then
+        echo Running on Apple Silicon...
+    else
+        exit 1
+    fi
+}
+
+is_macos?
+is_arm64?
+
+if [ -e /opt/homebrew/Cellar/node/15.8.0/ ]; then
+    cp -f ./node /opt/homebrew/Cellar/node/15.8.0/bin/node
+fi
+echo Completed!
